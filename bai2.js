14b28186eb3c15327e1a308452fdf98e6eda448d
ChatNghe <namnt98@gmail.com>
2022-10-10 10:01:36 +0700

    thaydoi1

---
+++ bai2.js
@@ -0,0 +1,4 @@
+let width = 50
+let height = 60
+let area = width * height
+document.write('Area = ' + area)
---
+++ bai3.js
@@ -0,0 +1,7 @@
+inputa = prompt("Enter a");
+inputb = prompt("Enter b");
+let a = parseInt(inputa);
+let b = parseInt(inputb);
+if((a % b) == 0){
+    alert("a là bội số của b ");
+}else alert("a không là bội số của b ");
